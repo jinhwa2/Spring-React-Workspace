66da9ca153680647b1a8e6619d220128da24e19b
jinhwa2 <162092896+jinhwa2@users.noreply.github.com>
2024-08-09 14:26:33 +0900

    profile image

--- image-front/src/component/Board.js
+++ image-front/src/component/Board.js
@@ -3,6 +3,9 @@ import axios from "axios";
 import '../App.css';
 
 const Board = () => {
+
+  const uploadAPI = "http://localhost:9007/gellery/upload";
+  const postAPI =  "http://localhost:9007/posts";
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   const [files, setFiles] = useState([]);
@@ -19,7 +22,7 @@ const Board = () => {
     formData.append("content", content);
 
     // 자바 컨트롤러에 데이터 전송! Post
-    axios.post("http://localhost:9007/gellery/upload", formData, {
+    axios.post(uploadAPI, formData, {
       headers: {
         // 전송할 데이터에 글자가 아닌 파일이 함께 전송된다 머릿말로 알려주기
         "Content-Type": "multipart/form-data",
@@ -33,7 +36,7 @@ const Board = () => {
   // const 기능을 작성해놓고 필요할 때 기능을 사용하기 위해 설정
   const 게시물가져오기 = () => {
     axios
-      .get("http://localhost:9007/posts") //자바 컨트롤러 url api 주소에서 데이터 가져오기
+      .get(postAPI) //자바 컨트롤러 url api 주소에서 데이터 가져오기
       .then((response) => {
         setPosts(response.data);
         console.log(response.data);
