d94d9ad7e2c171998138035eb4ffaea372a4424b
jinhwa2 <162092896+jinhwa2@users.noreply.github.com>
2024-08-19 11:58:09 +0900

    api

---
+++ front-xml-api/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import AirPollutionData from './AirPollutionData';
+
+function App() {
+  return (
+    <div className="App-header">
+      <h1>공기오염 조회 서비스</h1>
+      <AirPollutionData />
+    </div>
+  );
+}
+
+export default App;
